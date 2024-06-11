4ae9edc119bd24c861b6f4644e244a1f10177e33
Della fitria lestari <dellafitrialestari777@gmail.com>
2024-06-11 14:25:18 +0700

    Update nav

--- app/src/main/java/com/della_irvianti/eye_app/HomeActivity.java
+++ app/src/main/java/com/della_irvianti/eye_app/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.della_irvianti.eye_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Cari fitur1 menggunakan id dan tambahkan klik listener
+        LinearLayout fitur1 = findViewById(R.id.fitur1);
+        fitur1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Buat Intent untuk memulai TestActivity
+                Intent intent = new Intent(HomeActivity.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
