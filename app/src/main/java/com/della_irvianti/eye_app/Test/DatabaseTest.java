3885fd9d0638a64099a14c43573286fada42e4da
Della fitria lestari <dellafitrialestari777@gmail.com>
2024-06-17 12:12:13 +0700

    Update soal tes

--- app/src/main/java/com/della_irvianti/eye_app/Test/DatabaseTest.java
+++ app/src/main/java/com/della_irvianti/eye_app/Test/DatabaseTest.java
@@ -8,62 +8,62 @@ public class DatabaseTest {
             R.drawable.b1,
             R.drawable.b2,
             R.drawable.b3,
+            R.drawable.b4,
             R.drawable.b5,
+            R.drawable.b6,
             R.drawable.b7,
             R.drawable.b8,
             R.drawable.b9,
-            R.drawable.b13,
-            R.drawable.b18,
-            R.drawable.b23
+            R.drawable.b10
     };
 
     String[] answers = {
             "12",
             "8",
-            "6",
-            "57",
-            "3",
+            "29",
             "15",
-            "74",
+            "5",
+            "Tidak ada angka",
+            "26",
             "45",
-            "tidak ada angka",
+            "74",
             "42"
     };
 
     String[] penjelasan = {
             "Orang yang memiliki mata normal dan buta warna dapat melihat angka 12.",
             "Orang yang memiliki mata normal akan melihat angka 8.",
-            "Orang yang memiliki mata normal akan melihat angka 6.",
-            "Orang yang memiliki mata normal akan melihat angka 57.",
-            "Orang yang memiliki mata normal akan melihat angka 3.",
+            "Orang yang memiliki mata normal akan melihat angka 29.",
             "Orang yang memiliki mata normal akan melihat angka 15.",
-            "Orang yang memiliki mata normal akan melihat angka 74.",
-            "Orang yang memiliki mata normal akan melihat angka 45.",
+            "Orang yang memiliki mata normal akan melihat angka 5.",
             "Orang yang memiliki mata normal tidak akan melihat angka.",
+            "Orang yang memiliki mata normal akan melihat angka 26.",
+            "Orang yang memiliki mata normal akan melihat angka 45.",
+            "Orang yang memiliki mata normal akan melihat angka 74.",
             "Orang yang memiliki mata normal akan melihat angka 42."
     };
 
     String[] penjelasan2 = {
-            " ",
-            "Orang yang buta warna Merah-Hijau akan melihat angka 3.",
-            "Orang yang buta warna Merah-Hijau akan melihat angka 5.",
-            "Orang yang buta warna Merah-Hijau akan melihat angka 35.",
-            "Orang yang buta warna Merah-Hijau akan melihat angka 5.",
+            "Orang yang buta warna Merah-Hijau mungkin kesulitan atau tidak dapat melihat angka sama sekali",
+            "Orang yang buta warna Merah-Hijau akan melihat angka 6.",
+            "Orang yang buta warna Merah-Hijau akan melihat angka 7 atau 5.",
             "Orang yang buta warna Merah-Hijau akan melihat angka 17.",
-            "Orang yang buta warna Merah-Hijau akan melihat angka 21.",
             "Orang yang buta warna Merah-Hijau tidak melihat angka.",
             "Orang yang buta warna Merah-Hijau akan melihat angka 5.",
+            "Orang yang buta warna Merah-Hijau mungkin kesulitan atau tidak dapat melihat angka sama sekali",
+            "Orang yang buta warna Merah-Hijau tidak melihat angka.",
+            "Orang yang buta warna Merah-Hijau akan melihat angka 21.",
             "Orang yang mengalami protanopia atau protanomaly akan melihat angka 2."
     };
     String[] penjelasan3 = {
+            "Orang yang mengalami deuteranopia atau deuteranomaly akan melihat angka 2",
+            "Orang yang mengalami deuteranopia atau deuteranomaly mungkin melihat angka 8 dengan sedikit kabur",
+            "Orang mengalami deuteranopia atau deuteranomaly akan melihat angka 2 atau 0",
+            "Orang yang mengalami deuteranopia atau deuteranomaly mungkin melihat angka 15 dengan sedikit kabur",
+            "Orang mengalami deuteranomaly akan melihat angka 2",
             "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
+            "Orang mengalami deuteranopia atau deuteranomaly akan melihat angka 6 atau 2",
+            "Orang mengalami deuteranopia atau deuteranomaly akan melihat angka 5 atau 74",
             "",
             "Orang yang mengalami deuteranopia atau deuteranomaly akan melihat angka 4."
     };
